52636e6a2da2726cc8619194cb7505ba3f2139f5
JOAO XAVIER <joaoxneto7050@gmail.com>
2023-10-20 00:26:04 -0300

    ElasticBeanstalk  ECR

--- Form-SRE/IaC/Docker_Elastic_BeanStalk/env/homolog/backend.tf
+++ Form-SRE/IaC/Docker_Elastic_BeanStalk/env/homolog/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-jx"
-    key    = "Prod/terraform.tfstate"
+    key    = "homolog/terraform.tfstate"
     region = "us-west-2"
   }
 }
---
+++ Form-SRE/IaC/Docker_Elastic_BeanStalk/infra/ecr.tf
@@ -0,0 +1,3 @@
+resource "aws_ecr_repository" "tf-jx-ecr-project" {
+  name                 = var.nome
+}
